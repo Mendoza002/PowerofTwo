2fcb22c1774c2f714977f017b13c5ea03416f4e8
Bryan Mendoza <mendozatech@outlook.com>
2020-12-15 12:51:49 -0800

    finish structured english

---
+++ random_number_game.py
@@ -0,0 +1,21 @@
+"""
+This program will serve as a form of entertainment for the user.
+
+A random number will first be selected between 0 and 100.
+The user is allowed an unlimited number of guesses to guess the correct number.
+
+If the user guesses a lower amount than the selected number, the program will print to
+the user that the guess is "too low."
+
+If the user guesses a higher amount than the selected number, the program will print to
+the user that the guess is "too high."
+
+If the user guesses a lower amount than zero or a higher amount than one hundred, the program will print to the user that
+the guess is "invalid."
+
+If the user inputs their guess in a word format, the program will print to the user
+"You did not enter a number".
+
+Once the user guesses the correct number, the program will print to the user "Correct!"
+and will report how many guesses they attempted.
+"""
